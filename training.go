b6c80fc671fb77c8f79eca76f27ad62ab72b4ec7
mlange-42 <martin_lange_@gmx.net>
2024-07-16 20:35:03 +0200

    improve columns check

--- training.go
+++ training.go
@@ -1,19 +1,25 @@
 package som
 
+import "fmt"
+
 type Trainer struct {
 	som   *Som
 	table *Table
 }
 
-func NewTrainer(som *Som, table *Table) *Trainer {
+func NewTrainer(som *Som, table *Table) (*Trainer, error) {
 	t := &Trainer{
 		som:   som,
 		table: table,
 	}
-	t.checkTable()
-	return t
+	if !t.checkTable() {
+		return nil, fmt.Errorf("table columns do not match SOM layer columns")
+	}
+	return t, nil
 }
 
+// checkTable checks that the table columns match the SOM layer columns.
+// It returns true if the table columns match the SOM layer columns, false otherwise.
 func (t *Trainer) checkTable() bool {
 	if len(t.table.columns) != t.som.offset[len(t.som.offset)-1]+len(t.som.layers[len(t.som.layers)-1].columns) {
 		return false
--- training_test.go
+++ training_test.go
@@ -6,25 +6,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTrainerCheckTable(t *testing.T) {
+func TestNewTrainer(t *testing.T) {
 	som := New(Size{2, 3}, []LayerDef{
 		{Columns: []string{"x", "y"}},
 		{Columns: []string{"a", "b", "c"}},
 	})
 
 	t1 := NewTable([]string{"x", "y", "a", "b", "c"}, 5)
-	tr := NewTrainer(&som, &t1)
-	assert.True(t, tr.checkTable())
+	_, err := NewTrainer(&som, &t1)
+	assert.Nil(t, err)
 
 	t1 = NewTable([]string{"x", "y", "a", "b"}, 5)
-	tr = NewTrainer(&som, &t1)
-	assert.False(t, tr.checkTable())
+	_, err = NewTrainer(&som, &t1)
+	assert.NotNil(t, err)
 
 	t1 = NewTable([]string{"x", "y", "a", "b", "c", "d"}, 5)
-	tr = NewTrainer(&som, &t1)
-	assert.False(t, tr.checkTable())
+	_, err = NewTrainer(&som, &t1)
+	assert.NotNil(t, err)
 
 	t1 = NewTable([]string{"x", "a", "y", "b", "c"}, 5)
-	tr = NewTrainer(&som, &t1)
-	assert.False(t, tr.checkTable())
+	_, err = NewTrainer(&som, &t1)
+	assert.NotNil(t, err)
 }
