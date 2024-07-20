f29d64eb83058d3952c704b9f69f070dcf588f8c
mlange-42 <martin_lange_@gmx.net>
2024-07-20 23:39:06 +0200

    modify example sh files

--- _examples/cluster2d/run.sh
+++ _examples/cluster2d/run.sh
@@ -2,9 +2,9 @@ mkdir out
 som train untrained.yml data.csv -v 0.0 > out/trained.yml
 som plot heatmap out/trained.yml out/heatmap.png --data-file data.csv --labels class
 som plot u-matrix out/trained.yml out/u-matrix.png --data-file data.csv --labels class
-som plot xy out/trained.yml out/xy.png -x x -y y
+som plot xy out/trained.yml out/xy.png -x x -y y --data-file data.csv -C class
 
 som train untrained.yml data.csv > out/trained-vi.yml
 som plot heatmap out/trained-vi.yml out/heatmap-vi.png --data-file data.csv --labels class
 som plot u-matrix out/trained-vi.yml out/u-matrix-vi.png --data-file data.csv --labels class
-som plot xy out/trained-vi.yml out/xy-vi.png -x x -y y
+som plot xy out/trained-vi.yml out/xy-vi.png -x x -y y --data-file data.csv -C class
--- _examples/iris/run.sh
+++ _examples/iris/run.sh
@@ -2,4 +2,4 @@ mkdir out
 som train untrained.yml data.csv > out/trained.yml
 som plot heatmap out/trained.yml out/heatmap.png --data-file data.csv --labels species
 som plot u-matrix out/trained.yml out/u-matrix.png --data-file data.csv --labels species
-som plot xy out/trained.yml out/xy.png -x sepal_width -y petal_length -c species
+som plot xy out/trained.yml out/xy.png -x sepal_width -y petal_length -c species --data-file data.csv -C species
