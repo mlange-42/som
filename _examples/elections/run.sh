131bc9849a72a54d36dcf80fc6041ea361237f71
mlange-42 <martin_lange_@gmx.net>
2024-07-23 12:34:33 +0200

    Add example for German parliament elections

---
+++ _examples/elections/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+mkdir out
+set -e
+
+# Train and plot using simple SOM
+som train untrained.yml data.csv -v 0.0 > out/trained.yml
+som plot heatmap out/trained.yml out/heatmap.png --data-file data.csv --labels land
+som plot u-matrix out/trained.yml out/u-matrix.png --data-file data.csv --labels land
+som plot xy out/trained.yml out/xy-cdu-spd.png -x CDU -y SPD --data-file data.csv -C land
+som plot xy out/trained.yml out/xy-afd-gruene.png -x AfD -y GRUENE --data-file data.csv -C land
+
+# Train and plot using ViSOM
+som train untrained.yml data.csv > out/trained-vi.yml
+som plot heatmap out/trained-vi.yml out/heatmap-vi.png --data-file data.csv --labels land
+som plot u-matrix out/trained-vi.yml out/u-matrix-vi.png --data-file data.csv --labels land
+som plot xy out/trained-vi.yml out/xy-cdu-spd-vi.png -x CDU -y SPD --data-file data.csv -C land
+som plot xy out/trained-vi.yml out/xy-afd-gruene-vi.png -x AfD -y GRUENE --data-file data.csv -C land
