9b8c29fed145a4bdb31530685f4d3ba85c916f4a
mlange-42 <martin_lange_@gmx.net>
2024-07-24 01:43:29 +0200

    structure codes plot subcommands

--- _examples/iris/run.sh
+++ _examples/iris/run.sh
@@ -6,6 +6,6 @@ set -e
 
 som train untrained.yml data.csv > out/trained.yml
 som plot heatmap out/trained.yml out/heatmap.png --data-file data.csv --labels species
-som plot codes out/trained.yml out/codes.png
+som plot codes line out/trained.yml out/codes.png
 som plot u-matrix out/trained.yml out/u-matrix.png --data-file data.csv --labels species
 som plot xy out/trained.yml out/xy.png -x sepal_width -y petal_length -c species --data-file data.csv -C species
--- cmd/som/cli/plot_codes.go
+++ cmd/som/cli/plot_codes.go
@@ -1,28 +1,27 @@
 package cli
 
 import (
+	"context"
 	"fmt"
 	"image"
 
+	"github.com/mlange-42/som"
 	"github.com/mlange-42/som/plot"
 	"github.com/spf13/cobra"
 )
 
 func plotCodesCommand() *cobra.Command {
-	var size []int
-	var columns []string
-	var normalized bool
+	cliArgs := codePlotArgs{}
 
 	command := &cobra.Command{
 		Use:   "codes [flags] <som-file> <out-file>",
 		Short: "Plots SOM nodes codes in different ways",
 		Long:  `Plots SOM nodes codes in different ways`,
-		Args:  cobra.ExactArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			somFile := args[0]
-			outFile := args[1]
+			cliArgs.OutFile = args[1]
 
-			if len(size) != 2 {
+			if len(cliArgs.Size) != 2 {
 				return fmt.Errorf("size must be two integers")
 			}
 
@@ -31,26 +30,63 @@ func plotCodesCommand() *cobra.Command {
 				return err
 			}
 
-			_, indices, err := extractIndices(s, columns, false)
+			cliArgs.Som = s
+
+			ctx := context.WithValue(cmd.Context(), codePlotKey{}, cliArgs)
+			cmd.SetContext(ctx)
+
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			_ = cmd.Help()
+		},
+	}
+
+	command.PersistentFlags().StringSliceVarP(&cliArgs.Columns, "columns", "c", nil, "Columns to use for the heatmap (default all)")
+	command.PersistentFlags().BoolVarP(&cliArgs.Normalized, "normalized", "n", false, "Use raw, normalized node weights")
+	command.PersistentFlags().IntSliceVarP(&cliArgs.Size, "size", "s", []int{600, 400}, "Size of individual heatmap panels")
+	command.PersistentFlags().SortFlags = false
+
+	command.AddCommand(plotCodesLinesCommand())
+
+	return command
+}
+
+type codePlotArgs struct {
+	Som        *som.Som
+	OutFile    string
+	Columns    []string
+	Normalized bool
+	Size       []int
+}
+
+type codePlotKey struct{}
+
+func plotCodesLinesCommand() *cobra.Command {
+	command := &cobra.Command{
+		Use:   "line [flags] <som-file> <out-file>",
+		Short: "Plots SOM nodes codes as line charts",
+		Long:  `Plots SOM nodes codes as line charts`,
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliArgs, ok := cmd.Context().Value(codePlotKey{}).(codePlotArgs)
+			if !ok {
+				return fmt.Errorf("args not found in context")
+			}
+
+			_, indices, err := extractIndices(cliArgs.Som, cliArgs.Columns, false)
 			if err != nil {
 				return err
 			}
 
 			plotType := plot.CodeLines{}
-			img, err := plot.Codes(s, indices, normalized, &plotType, image.Pt(size[0], size[1]))
+			img, err := plot.Codes(cliArgs.Som, indices, cliArgs.Normalized, &plotType, image.Pt(cliArgs.Size[0], cliArgs.Size[1]))
 			if err != nil {
 				return err
 			}
 
-			return writeImage(img, outFile)
+			return writeImage(img, cliArgs.OutFile)
 		},
 	}
-
-	command.Flags().StringSliceVarP(&columns, "columns", "c", nil, "Columns to use for the heatmap (default all)")
-	command.Flags().BoolVarP(&normalized, "normalized", "n", false, "Use raw, normalized node weights")
-	command.Flags().IntSliceVarP(&size, "size", "s", []int{600, 400}, "Size of individual heatmap panels")
-
-	command.Flags().SortFlags = false
-
 	return command
 }
